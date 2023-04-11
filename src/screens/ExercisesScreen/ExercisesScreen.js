3b43427f8c7a38395ee351b81cd01e5900888f52
Tori McDonald <torimickid@gmail.com>
2023-04-11 13:32:43 -0700

    category page basic UI

--- src/screens/ExercisesScreen/ExercisesScreen.js
+++ src/screens/ExercisesScreen/ExercisesScreen.js
@@ -1,25 +1,176 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+// THIS IS THE DISCOVER TAB
+
+import {View, Text, StyleSheet, FlatList, SectionList, Dimensions, Button} from 'react-native';
 import React from 'react';
 
-const ExercisesScreen = () => {
+// array holding all categories
+
+const sections = [
+  {
+    title: 'Cardio',
+    theme: "#FBC688", 
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  }, 
+  { 
+    title: 'Strength Training',
+    theme: "#CCD5AE", 
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  }, 
+  {
+    title: 'Flexibility',
+    theme: "#86BBD8",
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  }, 
+  {
+    title: 'Aerobics',
+    theme: "#D4A373", 
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  },
+  {
+    title: 'Endurance',
+    theme: "#85998A", 
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  },
+  {
+    title: 'Balance',
+    theme: "#D4A373", 
+    data: [
+    {
+      key: '1',
+      text: 'exercise 1',
+    },
+    {
+      key: '2',
+      text: 'exercise 2',
+    },
+    {
+      key: '3',
+      text: 'exercise 3',
+    }]
+  },
+];
+
+const ListItem = ({ item, theme }) => {
   return (
-    <View style={styles.container}>
-      <Text> move to exercise cards screen </Text>
-      <Button
-        title="View Exercise Cards"
-        onPress={alert('exercise cards pressed')}
-      />
+    <View style={[styles.item, {backgroundColor: theme}]}>  
+      <Text style={styles.itemText}>{item.text} </Text>
     </View>
-  )
+  );
+};
+
+const ExercisesScreen = () => {
+  return(
+    <>
+    <Text>Discover</Text>
+    <SectionList
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+        stickySectionHeadersEnabled={false}
+        sections={sections}
+        renderSectionHeader={({ section }) => (
+          <>
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+            <FlatList
+              data={section.data}
+              horizontal
+              style={styles.container}
+              renderItem={({ item }) => {
+                return <ListItem item={item} theme={section.theme}   />;
+              }}
+            />
+          </> 
+        )}
+        renderItem={({ item, section }) => {
+          return null
+        }}
+    />
+    </>
+  );
+    
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 25,
+    marginBottom: 25,
+    
+  },
+  item: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#85998a',
-  }
+    flex: 1,
+    // marginLeft: 15,
+    marginRight: 15,
+    height: 90,
+    width: 250,
+    borderRadius: 10,
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent',
+  },
+  itemText: {
+    color: '#fff',
+  },
 })
 
 export default ExercisesScreen;
