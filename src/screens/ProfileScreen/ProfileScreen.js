d91c939ab6a83e3689442c23f30137c7d089a8ae
Breanna <65743665+breanna-00@users.noreply.github.com>
2023-04-13 16:43:15 -0700

    modified files

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,10 +1,18 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const ProfileScreen = () => {
+const Stack = createStackNavigator();
+
+const ProfileScreen = (navigation) => {
   return (
     <View style={styles.container}>
       <Text>ProfileScreen</Text>
+      <Button 
+          title = "Cardio"
+          onPress = {() => navigation.navigate('Cardio')} 
+          color='#85998A'/>
     </View>
   );
 };
--- src/screens/ProfileScreen/pushNoti.js
+++ src/screens/ProfileScreen/pushNoti.js
@@ -1,28 +1,52 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import DateTimePicker from './DateTimePicker';
-import DaysOfWeekPicker from './DaysOfTheWeek';
-
-const scheduledNoti = {
-    time: NOT_INITIALIZED_ERROR, 
-    date: NOT_INITIALIZED_ERROR
-}
+import DaysOfWeekPicker from './DaysOfWeekPicker';
+import moment from 'moment';
 
 const PushNoti = () => {
-return (
-    <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+  const [time, setTime] = useState(null);
+  const [days, setDays] = useState(null);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [showDaysPicker, setShowDaysPicker] = useState(false);
+
+  const handleTimeChange = (date) => {
+    setTime(moment(date).format('HH:mm'));
+  };
+
+  const handleDaysChange = (updatedDays) => {
+    setDays(updatedDays);
+  };
+
+  const handleSave = () => {
+    // Save the selected time and days here
+    console.log('Selected Time:', time);
+    console.log('Selected Days:', days);
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={() => setShowTimePicker(true)}>
+        <Text>{time || 'Select Time'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setShowDaysPicker(true)}>
+        <Text>{days ? 'Days Selected' : 'Select Days'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleSave}>
+        <Text>Save</Text>
+      </TouchableOpacity>
+      <DateTimePicker
+        visible={showTimePicker}
+        onClose={() => setShowTimePicker(false)}
+        onDateChange={handleTimeChange}
+      />
+      <DaysOfWeekPicker
+        onDaysChange={handleDaysChange}
+        visible={showDaysPicker}
+        onClose={() => setShowDaysPicker(false)}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ffe289',
-  }
-})
 
 export default PushNoti;
