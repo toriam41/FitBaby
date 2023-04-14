f2cad2aa066b5512a56ad9144cd7dbc973e13d7c
Breanna <65743665+breanna-00@users.noreply.github.com>
2023-04-13 21:46:18 -0700

    checkboxes are labelled

--- src/screens/RoutineScreen/DaysOfWeekPicker.js
+++ src/screens/RoutineScreen/DaysOfWeekPicker.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 
 const DaysOfWeekPicker = ({ onDaysChange }) => {
@@ -21,43 +21,72 @@ const DaysOfWeekPicker = ({ onDaysChange }) => {
 
   return (
     <View>
-      <CheckBox
-        value={days.sun}
-        onValueChange={() => handleCheck('sun')}
-        label="Sunday"
-      />
-      <CheckBox
-        value={days.mon}
-        onValueChange={() => handleCheck('mon')}
-        label="Monday"
-      />
-      <CheckBox
-        value={days.tue}
-        onValueChange={() => handleCheck('tue')}
-        label="Tuesday"
-      />
-      <CheckBox
-        value={days.wed}
-        onValueChange={() => handleCheck('wed')}
-        label="Wednesday"
-      />
-      <CheckBox
-        value={days.thu}
-        onValueChange={() => handleCheck('thu')}
-        label="Thursday"
-      />
-      <CheckBox
-        value={days.fri}
-        onValueChange={() => handleCheck('fri')}
-        label="Friday"
-      />
-      <CheckBox
-        value={days.sat}
-        onValueChange={() => handleCheck('sat')}
-        label="Saturday"
-      />
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          value={days.sun}
+          onValueChange={() => handleCheck('sun')}
+          label="Sunday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Sun</Text>
+        <CheckBox
+          value={days.mon}
+          onValueChange={() => handleCheck('mon')}
+          label="Monday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Mon</Text>
+        <CheckBox
+          value={days.tue}
+          onValueChange={() => handleCheck('tue')}
+          label="Tuesday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Tue</Text>
+        <CheckBox
+          value={days.wed}
+          onValueChange={() => handleCheck('wed')}
+          label="Wednesday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Wed</Text>
+        <CheckBox
+          value={days.thu}
+          onValueChange={() => handleCheck('thu')}
+          label="Thursday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Thu</Text>
+        <CheckBox
+          value={days.fri}
+          onValueChange={() => handleCheck('fri')}
+          label="Friday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Fri</Text>
+        <CheckBox
+          value={days.sat}
+          onValueChange={() => handleCheck('sat')}
+          label="Saturday"
+          style={styles.checkbox}
+        />
+        <Text styles={styles.label}>Sat</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    margin: 8,
+  },
+});
 
 export default DaysOfWeekPicker;
