19e3fc17273003afb83a33199d9a90006db68519
Breanna <65743665+breanna-00@users.noreply.github.com>
2023-04-13 17:19:36 -0700

    Update ProfileScreen.js

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,18 +1,10 @@
 import {View, Text, StyleSheet, Button} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
-
-const ProfileScreen = (navigation) => {
+const ProfileScreen = () => {
   return (
     <View style={styles.container}>
       <Text>ProfileScreen</Text>
-      <Button 
-          title = "Cardio"
-          onPress = {() => navigation.navigate('Cardio')} 
-          color='#85998A'/>
     </View>
   );
 };
