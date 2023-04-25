363fadf0c26403604266be32ee2200908b15f9b0
Breanna <65743665+breanna-00@users.noreply.github.com>
2023-04-25 13:58:12 -0700

    modified files

--- src/screens/RoutineScreen/RoutineScreen.js
+++ src/screens/RoutineScreen/RoutineScreen.js
@@ -13,28 +13,6 @@ import DateTimePicker from 'react-native-modal-datetime-picker';
 
 const RoutineScreen = ({navigation, route}) => {
   const [routineList, setRoutineList] = useState([]);
-  const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-  const [text, setText] = useState('Empty');
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'android');
-    setDate(currentDate);
-
-    let tempDate = new Date(currentDate);
-    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
-    let fTime = 'Hours: ' + tempDate.getHours() + ' | Minutes: ' + tempDate.getMinutes();
-    setText(fDate + '\n' + fTime);
-
-    console.log(fDate + ' (' + fTime + ')')
-  }
-  
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  }
 
   // Receive exercise from params and add it to the routineList
   useEffect(() => {
@@ -94,21 +72,6 @@ const RoutineScreen = ({navigation, route}) => {
   //add exercise button shall navigate to exercises screen
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight:'bold', fontSize:20}}>{text}</Text>
-      <View style={{margin:20}}>
-      <Button title='TimePicker' onPress={() => showMode('time')}/>
-      </View>
-
-      {show && (
-        <DateTimePicker
-        testID='dateTimePicker'
-        value={date}
-        mode={mode}
-        is24Hour={true}
-        display='default'
-        onChange={onChange}
-      />)}
-
       <View style={styles.header}>
         <Text style={styles.headerText}>My Routine</Text>
         <Button
