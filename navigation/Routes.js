be6911b4cee992db71c61890313dbf9e69325904
Breanna <65743665+breanna-00@users.noreply.github.com>
2023-05-09 13:46:20 -0700

    comment Route

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -2,6 +2,7 @@ import AuthStack from "./AuthStack";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
+// Allows navigation between screens
 const Routes = () => {
     return (
         <NavigationContainer>
